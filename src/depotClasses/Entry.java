b008f25c1109f5a0adbe08441d2a773da2643f94
Karl Fitzgibbons <up619458@myport.ac.uk>
2018-03-20 12:54:02 +0000

    entry depot select

--- src/depotClasses/Entry.java
+++ src/depotClasses/Entry.java
@@ -1,8 +1,46 @@
 package depotClasses;
-
 import java.util.Scanner;
 
 public class Entry {
-	Scanner sc = new Scanner(System.in);
-	int i = sc.nextInt();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String menuNav=sc.nextLine();
+		do {
+			System.out.println("Pelase select a Depot");	
+			
+			System.out.printf("\n1- [L]iverpool");
+			System.out.printf("\n2- [M]anchester");
+			System.out.printf("\n3- [B]ristol");
+			System.out.printf("\nQ- Quit");
+			System.out.printf("\nPick:");
+			//spacing (both in code and programme) for readability.
+			
+			
+			
+			switch (menuNav.toUpperCase()) {	
+				case "1":
+				case "L":	
+					{}
+					break;
+				case "2": 
+				case "M":	
+					{}
+					break;
+				case "3": 
+				case "B":
+					{}
+					break;
+				case "Q":
+					{System.exit(0);}
+					break;
+				default: System.out.println("not recognised, please try again");
+					break;
+			}
+			
+			}while(!"Q".equals(menuNav));
+		sc.close();
+	
+
+}
 }
