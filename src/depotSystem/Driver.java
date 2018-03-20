eca1bbeaa742af1cd0957bd579696e4ec26016cc
Karl Fitzgibbons <up619458@myport.ac.uk>
2018-03-20 13:38:29 +0000

    login mayebish

--- src/depotSystem/Driver.java
+++ src/depotSystem/Driver.java
@@ -1,20 +1,38 @@
 package depotSystem;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Driver {
  private String userName;
- private static String password;
+ private  String password;
 
 
  public Driver(String userName, String password, String jobState){
 	 this.userName= userName;
-	 Driver.password= password;  
+	 this.password= password;  
  };
  
- public static boolean checkPassword(String checkPass){
-	 if (checkPass.equals(password))
-	 	{return true;
-	 	}
-	 else return false;
+ public static boolean checkPassword(String username, String pass) throws FileNotFoundException{
+boolean passOkay = false;
+		@SuppressWarnings("rawtypes")
+		LinkedList list= new LinkedList();
+ 
+	 Scanner sc = new Scanner(new FileReader("M:\\data\\drivers"));
+		String next;
+	 while (sc.hasNextLine()){
+			next = sc.nextLine();
+			list.add(next);
+			if(next.equals(username)){
+				if (sc.next().split(" ").equals(pass)){ return passOkay=true;}
+				else return passOkay=false;}
+			return passOkay;
+			}
+		sc.close();
+		return passOkay;
+		
  }
  
  public String getUser(){
