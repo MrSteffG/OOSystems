9eacfa47e48f4b9e2b06af1a7503488e61294463
Karl Fitzgibbons <up619458@myport.ac.uk>
2018-03-20 11:49:27 +0000

    delete depot

--- src/depotSystem/Depot.java
+++ src/depotSystem/Depot.java
@@ -1,26 +1,9 @@
 package depotSystem;
 
 public class Depot {
-	private String depotName;
 
-	public Depot(String depotName){
-		this.depotName=depotName;
-	};
-
-public void logOn(String userName, String pass){
-	get.username
-	
-	
-};
-
-
-
-
-
-
-
+	public Depot() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
-	
-	
-
