742c135e0f5c7bbe4257b97ebdf67431ef786f62
Karl Fitzgibbons <up619458@myport.ac.uk>
2018-04-03 12:36:19 +0100

    by jove

--- src/depotClasses/Sys.java
+++ src/depotClasses/Sys.java
@@ -1,7 +1,7 @@
 package depotClasses;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import depotSystem.Depot;
@@ -9,14 +9,14 @@ import depotSystem.Driver;
 import depotSystem.Vehicle;
 
 public class Sys {
-	private static Depot[] depotArray;
-	private static Driver[] arrayDriver;
-	private static Vehicle[] arrayVehicle;
-	private static String depotName;
-	private static Depot liverpool;
-	private static Depot birmingham;
-	private static Depot manchester;
+private static String depotName;
+private static Depot liverpool;
+private static Depot manchester;
+private static Depot birmingham;
+private static Depot[] depotArray;
+	
 
+	
 	public static void run() throws FileNotFoundException {
 
 		System.out.println("Pelase select a Depot");
@@ -45,6 +45,7 @@ public class Sys {
 		}
 			break;
 		case "Q": {
+			sc.close();
 			System.exit(0);
 		}
 			break;
@@ -54,72 +55,21 @@ public class Sys {
 		}
 	}
 
-	public static Depot getDepot(String depotChoice) {
-		arrayVehicle = new Vehicle[6];
-		arrayDriver = new Driver[5];
+	private static Depot getDepot(String depotChoice) {
+		depotArray= new Depot[3];
+		depotArray[0]=Depot.buildLiv();
+		depotArray[1]= Depot.buildMan();
+		depotArray[2]= Depot.buildBirm();
 
-		depotName = "liverpool";
-		arrayDriver[0] = new Driver("Steve", "donkeys");
-		arrayDriver[1] = new Driver("Pete", "cows1");
-		arrayDriver[2] = new Driver("Dan", "mongoose2");
+		 for (Depot currentDepot : depotArray) {
+	            if (currentDepot.getDepotName() == depotChoice) {
+	            	System.out.println(currentDepot.getDepotName());
+	                return currentDepot;
+	            }
+	        }
 
-		arrayVehicle[0] = new Vehicle("CS1457", "DAYCAB", "WHALEBODY");
-		arrayVehicle[1] = new Vehicle("CS1458", "VOLVO", "FM7290");
-		arrayVehicle[2] = new Vehicle("CS1451", "Merce;des-Benz", "Actros");
-		arrayVehicle[3] = new Vehicle("CS1452", "Iveco", "PowerStar420E5");
-		liverpool = new Depot(depotName, arrayDriver, arrayVehicle);
+	        return null;
 
-		depotName = "birmingham";
-		arrayDriver[0] = new Driver("milo", "madeitwith");
-		arrayDriver[1] = new Driver("rao", "morrocco123");
-		arrayDriver[2] = new Driver("Hilary", "emailpass");
-		arrayDriver[3] = new Driver("thedon", "nogunlaw");
-
-		arrayVehicle[0] = new Vehicle("CS1461", "Tankerman", "Wetdrive");
-		arrayVehicle[1] = new Vehicle("CS1576", "Tankerman", "moistroller");
-		arrayVehicle[2] = new Vehicle("CS1455", "Hyundai", "Xcient");
-		arrayVehicle[3] = new Vehicle("CS1456 ", "Volvo", "VN780");
-		birmingham = new Depot(depotName, arrayDriver, arrayVehicle);
-
-		depotName = "manchester";
-		arrayDriver[0] = new Driver("Jeremy", "ilovesting");
-		arrayDriver[1] = new Driver("sally", "trucks4life");
-		arrayDriver[2] = new Driver("ruairi", "iloveanime");
-		arrayDriver[3] = new Driver("mark", "not93");
-
-		arrayVehicle[0] = new Vehicle("CS1459", "Tankerman", "Tankomatic5000");
-		arrayVehicle[1] = new Vehicle("CS1460", "Tankerman", "Tankotron4");
-		arrayVehicle[2] = new Vehicle("CS1453", "Tata", "Prima");
-		arrayVehicle[3] = new Vehicle("CS1454", "Foton", "Auman");
-		manchester = new Depot(depotName, arrayDriver, arrayVehicle);
-
-		depotArray = new Depot[5];
-		depotArray[0] = birmingham;
-		depotArray[1] = liverpool;
-		depotArray[2] = manchester;
-
-		for (Depot currentDepot : depotArray) {
-			if (Depot.getDepotName() == depotChoice) {
-				System.out.println(Depot.getDepotName());
-				return currentDepot;
-			}
-		}
-		return null;
 	}
 }
 
-/*
- * Scanner driveSc = new Scanner(new FileReader(depotChoice + "Drivers.txt"));
- * Scanner tankSc = new Scanner(new FileReader(depotChoice + "Tankers.txt"));
- * Scanner truckSc=new Scanner(new FileReader(depotChoice+ "Trucks.txt"));
- * Driver[] D= new Driver[100]; int i=0; while(driveSc.hasNext()){ String[]
- * data= driveSc.nextLine().split(" "); D[i]= new Driver(data[0],(data[1]));
- * i++; } Vehicle[] V= new Vehicle[100]; int j=0; while(tankSc.hasNext()){
- * String[] data= tankSc.nextLine().split(" "); V[j]= new
- * Vehicle(data[0],(data[1]),(data[2])); j++; } while(truckSc.hasNext()){
- * String[] data= truckSc.nextLine().split(" "); V[j]= new
- * Vehicle(data[0],(data[1]),(data[2])); j++;
- * 
- * } System.out.println(Depot.getDepotName()); return new
- * Depot(depotChoice,D,V);
- */
--- src/depotSystem/Depot.java
+++ src/depotSystem/Depot.java
@@ -1,65 +1,112 @@
 package depotSystem;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Depot {
-	private static String depotName;
+	private String depotName;
 	private Driver[] arrayDriver;
 	private Vehicle[] arrayVehicle;
 	
-	
-public Depot(String depotName, Driver[] arrayDriver, Vehicle[] arrayVehicle){
-	this.depotName=depotName;
-	this.arrayDriver=arrayDriver;
-	this.arrayVehicle=arrayVehicle;
-	
+	public Depot(String depotName, Driver[] arrayDriver, Vehicle[] arrayVehicle) {
+		this.depotName=depotName;
+		this.arrayDriver=arrayDriver;
+		this.arrayVehicle=arrayVehicle;
+
 	}
-	
-public boolean logon() {
-	Scanner sc=new Scanner(System.in);
-	
-     System.out.println("Please enter your Username : ");
-     String user = sc.next();
 
-     System.out.println("Please enter your PIN : ");
-     String password =sc.next();
-     
-     Driver.checkPassword(user,password);
+	public boolean logon() {
+		Scanner sc = new Scanner(System.in);
 
-     
-     
+		System.out.println("Please enter your Username : ");
+		String user = sc.next();
 
-  return false;   
-}
-		
+		System.out.println("Please enter your PIN : ");
+		String password = sc.next();
 
-	 
-public Vehicle GetVehicle(String regNo) {
-        for (Vehicle currentVehicle : arrayVehicle) {
-            if (currentVehicle.getRegNo() == regNo) {
-                return currentVehicle;
-            }
- }
-        return null;
-}
-		
-	 
-	 
-public Driver GetDriver(String userName) {
-	        for (Driver currentDriver : arrayDriver) {
-	            if (currentDriver.getUser() == userName) {
-	                return currentDriver;
-	            }
-	 }
-	        return null;
-}
-		
-public void SetupWorkSchedule() {
-		 
-	 }
-public static String getDepotName(){ 
+		Driver.checkPassword(user, password);
+		sc.close();
+
+		return false;
+	}
+
+	public Vehicle GetVehicle(String regNo) {
+		for (Vehicle currentVehicle : arrayVehicle) {
+			if (currentVehicle.getRegNo() == regNo) {
+				return currentVehicle;
+			}
+		}
+		return null;
+	}
+
+	public Driver GetDriver(String userName) {
+		for (Driver currentDriver : arrayDriver) {
+			if (currentDriver.getUser() == userName) {
+				return currentDriver;
+			}
+		}
+		return null;
+	}
+
+	public void SetupWorkSchedule() {
+
+	}
+
+	public String getDepotName() {
 		return depotName;
 	}
-}
 
+	public static Depot buildLiv() {
+	
+		Vehicle[] liverpoolVehicle = new Vehicle[4];
+		Driver[] liverpoolDriver = new Driver[3];
+
+		String name = "liverpool";
+		liverpoolDriver[0] = new Driver("Steve", "donkeys");
+		liverpoolDriver[1] = new Driver("Pete", "cows1");
+		liverpoolDriver[2] = new Driver("Dan", "mongoose2");
+
+		liverpoolVehicle[0] = new Vehicle("CS1457", "DAYCAB", "WHALEBODY");
+		liverpoolVehicle[1] = new Vehicle("CS1458", "VOLVO", "FM7290");
+		liverpoolVehicle[2] = new Vehicle("CS1451", "Merce;des-Benz", "Actros");
+		liverpoolVehicle[3] = new Vehicle("CS1452", "Iveco", "PowerStar420E5");
+		Depot liverpool = new Depot(name, liverpoolDriver, liverpoolVehicle);
+		return liverpool;
+	}
+
+	public static Depot buildMan() {
+		
+		Vehicle[] manchesterVehicle = new Vehicle[4];
+		Driver[] manchesterDriver = new Driver[4];
+		String name = "manchester";
+		manchesterDriver[0] = new Driver("Jeremy", "ilovesting");
+		manchesterDriver[1] = new Driver("sally", "trucks4life");
+		manchesterDriver[2] = new Driver("ruairi", "iloveanime");
+		manchesterDriver[3] = new Driver("mark", "not93");
+
+		manchesterVehicle[0] = new Vehicle("CS1459", "Tankerman", "Tankomatic5000");
+		manchesterVehicle[1] = new Vehicle("CS1460", "Tankerman", "Tankotron4");
+		manchesterVehicle[2] = new Vehicle("CS1453", "Tata", "Prima");
+		manchesterVehicle[3] = new Vehicle("CS1454", "Foton", "Auman");
+		Depot manchester = new Depot(name, manchesterDriver, manchesterVehicle);
+		return manchester;
+	}
+
+	public static Depot buildBirm() {
+		Vehicle[] birminghamVehicle = new Vehicle[4];
+		Driver[] birminghamDriver = new Driver[4];
+		String name = "birmingham";
+		birminghamDriver[0] = new Driver("milo", "madeitwith");
+		birminghamDriver[1] = new Driver("rao", "morrocco123");
+		birminghamDriver[2] = new Driver("Hilary", "emailpass");
+		birminghamDriver[3] = new Driver("thedon", "nogunlaw");
+
+		birminghamVehicle[0] = new Vehicle("CS1461", "Tankerman", "Wetdrive");
+		birminghamVehicle[1] = new Vehicle("CS1576", "Tankerman", "moistroller");
+		birminghamVehicle[2] = new Vehicle("CS1455", "Hyundai", "Xcient");
+		birminghamVehicle[3] = new Vehicle("CS1456", "Volvo", "VN780");
+		Depot birmingham = new Depot(name, birminghamDriver, birminghamVehicle);
+		return birmingham;
+
+	}
+
+}
